e213aab27cc4a837fbf78d8305562a0ed6422c03
Aadiac <aadithpsaadi@gmail.com>
2025-11-26 14:54:55 +0530

    fixed err

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This MUST match your repo name "/cloud-kitchen/" exactly
+  base: '/cloud-kitchen/', 
   server: {
     host: true,
+    // This is for your local testing via ngrok, you can keep it
     allowedHosts: ['untugged-forgetfully-ardith.ngrok-free.dev']
-  },
-  base: '/cloud-kitchen/',
+  }
 })
